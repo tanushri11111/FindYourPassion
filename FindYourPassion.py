f6614946891c9939619518de2288c1cb7cea63c0
tanushri11111 <73070364+tanushri11111@users.noreply.github.com>
2021-12-12 12:43:48 +0530

    Add files via upload

---
+++ FindYourPassion.py
@@ -0,0 +1,22 @@
+print("Find Your Passion In Life")
+
+Point=input("What is the Point Number-")
+
+if(Point == '1'):
+    print("Point 1: What are you Interested in ?")
+  
+elif(Point == '2'):
+    print("Point 2: Find Your Passion ?")
+    
+elif(Point == '3'):
+    print("Point 3: Practice")
+
+elif(Point == '4'):
+    print("Point 4: Be The Best ")
+    
+
+elif(Point == '5'):
+    print("Point 5: Achieve your Goal and find Success")
+
+if(Point >= '6'):
+    print("Follow only these five steps")
